9f95d6a4bfa09f759a51dea274a02f7e2b64e4b5
valselee <sollleky72@gmail.com>
2023-06-20 20:18:29 +0900

    [Init] Project Init

---
+++ MapKit_BareBones_SwiftUI/MapKit_BareBones_SwiftUI.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_BareBones_SwiftUIApp.swift
+//  MapKit_BareBones_SwiftUI
+//
+//  Created by Celan on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_BareBones_SwiftUI: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
